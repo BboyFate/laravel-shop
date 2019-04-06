7fe1b65eedcfc68b4964d6585a696a0d406943c4
Fate <bboyfate@163.com>
2019-04-06 16:41:44 +0800

    用户界面 - 订单列表页

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,5 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('cart/{product_sku}', 'CartController@remove')->name('cart.remove');
 
     Route::post('orders', 'OrdersController@store')->name('orders.store');
+    Route::get('orders', 'OrdersController@index')->name('orders.index');
 });
