2763fbc65d6cea1a462d9aa53bc6127a8c2742e1
Fate <bboyfate@163.com>
2019-04-09 07:44:37 +0800

    封装业务代码

---
+++ app/Services/OrderService.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Order;
+use App\Models\User;
+use App\Models\UserAddress;
+use App\Models\ProductSku;
+use App\Jobs\CloseOrder;
+use App\Exceptions\InvalidRequestException;
+use Carbon\Carbon;
+
+class OrderService
+{
+    public function store(User $user, UserAddress $userAddress, $remark, $items)
+    {
+        $order = \DB::transaction(function () use ($user, $userAddress, $remark, $items) {
+            // 更新地址最后使用时间
+            $userAddress->update(['last_used_at' => Carbon::now()]);
+            // 创建一个订单
+            $order = new Order([
+                'address' => [
+                    'address' => $userAddress->full_address,
+                    'zip' => $userAddress->zip,
+                    'contact_name' => $userAddress->contact_name,
+                    'contact_phone' => $userAddress->contact_phone,
+                ],
+                'remark' => $remark,
+                'total_amount' => 0,
+            ]);
+            $order->user()->associate($user);
+            $order->save();
+
+            $totalAmount = 0;
+            // 遍历用户提交的商品 SKU
+            foreach ($items as $data) {
+                $sku = ProductSku::find($data['sku_id']);
+                // 创建一个 OrderItem 并直接与当前订单关联
+                $item = $order->items()->make([
+                    'amount' => $data['amount'],
+                    'price' => $sku->price,
+                ]);
+                $item->product()->associate($sku->product_id);
+                $item->productSku()->associate($sku);
+                $item->save();
+                $totalAmount += $sku->price * $data['amount'];
+                if ($sku->decreaseStock($data['amount']) <= 0) {
+                    throw new InvalidRequestException('该商品库存不足');
+                }
+            }
+            // 更新订单总金额
+            $order->update(['total_amount' => $totalAmount]);
+
+            // 将下单的商品从购物车移除
+            $skuIds = collect($items)->pluck('sku_id')->all();
+            app(CartService::class)->remove($skuIds);
+
+            return $order;
+        });
+
+        // 这里直接使用 dispatch 函数，订单加入队列
+        dispatch(new CloseOrder($order, config('app.order_ttl')));
+
+        return $order;
+    }
+}
